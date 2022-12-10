772faa6b9b03ceb9015ddd8f4792aad6a7c3a249
WojKin1 <wk305371@student.polsl.pl>
2022-12-10 19:14:59 +0100

    #2 api_controllers
    controllers requests

---
+++ app/Http/Controllers/Api/V1/InvoicesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Invoice;
+use App\Models\Customer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreInvoicesRequest;
+use App\Http\Requests\UpdateInvoicesRequest;
+
+class InvoicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Customer::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreInvoicesRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreInvoicesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoices  $invoices
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoices $invoices)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invoices  $invoices
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invoices $invoices)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateInvoicesRequest  $request
+     * @param  \App\Models\Invoices  $invoices
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateInvoicesRequest $request, Invoices $invoices)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoices  $invoices
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoices $invoices)
+    {
+        //
+    }
+}
