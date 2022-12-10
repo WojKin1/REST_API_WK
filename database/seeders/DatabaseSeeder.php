548b53d5de64ea4734801088a0496c39bc3c74f3
WojKin1 <wk305371@student.polsl.pl>
2022-12-10 05:36:46 +0100

    rest api#1-db
    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use\App\Models\Customer;
 
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+
+    $this->call([
+        CustomerSeeder::class
+    ]);
         // \App\Models\User::factory(10)->create();
     }
 }
